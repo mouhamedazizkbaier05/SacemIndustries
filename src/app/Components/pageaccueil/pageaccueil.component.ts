a19904178f56f27d162ea00849e13432c3a735eb
aziz kbaier <azizm9592@gmail.com>
2024-02-07 16:50:49 +0100

    pageacceuil

---
+++ src/app/Components/pageaccueil/pageaccueil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pageaccueil',
+  standalone: true,
+  imports: [],
+  templateUrl: './pageaccueil.component.html',
+  styleUrl: './pageaccueil.component.css'
+})
+export class PageaccueilComponent {
+
+}
